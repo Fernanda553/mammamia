92a89dae6eb88b1b8d43277dc190c7bf54644f0f
fernanda nuñez <fernanda.nunez55@gmail.com>
2023-07-21 14:18:47 -0400

    add component Navigation and Views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,42 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import DataContext from "./context/DataContext";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+
 function App() {
-  return <></>;
+  const [data, setData] = useState([]);
+
+  const endpoint = "/pizzas.json";
+
+  const getData = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data.photos);
+  };
+
+  useEffect(() => {
+    getData(endpoint);
+  }, []);
+
+  const allState = {
+    data,
+    setData,
+  };
+  return (
+    <>
+      <DataContext value={allState}>
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path></Route>
+          </Routes>
+        </BrowserRouter>
+      </DataContext>
+    </>
+  );
 }
 
 export default App;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,23 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/carrito">🛒</NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default Navigation;
